14a471b77220ac91eda4267b1501608127fd96c1
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-12-06 00:59:07 +0630

    ReactFacts

---
+++ ReactFacts/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import MainContent from "./components/MainContent";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <MainContent />
+    </>
+  );
+}
+
+export default App;
