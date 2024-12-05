1dd09ee5c598bb8f03f5d41557e109fc53d27e09
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-12-06 00:06:51 +0630

    update first vite react

--- first-vite-react/src/main.jsx
+++ first-vite-react/src/main.jsx
@@ -3,16 +3,22 @@ import { createRoot } from "react-dom/client";
 createRoot(document.getElementById("root")).render(<TemporaryName />);
 function TemporaryName() {
   return (
-    <main>
-      <img src="src/assets/react-logo.png" width="40px" alt="react logo" />
-      <h1>Fun facts about React!</h1>
-      <ul>
-        <li>Was first release in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 200K stars on GitHub</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </main>
+    <div>
+      <header>
+        <nav>
+          <img src="src/assets/react-logo.png" width="40px" alt="react logo" />
+        </nav>
+      </header>
+      <main>
+        <h1>Fun facts about React!</h1>
+        <ul>
+          <li>Was first release in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 200K stars on GitHub</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+    </div>
   );
 }
