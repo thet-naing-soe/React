bdc4145e343d5f03cc1ce9566e0dad48d846624b
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-12-06 16:10:01 +0630

    travel journal

---
+++ travel-journal/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import Entry from "./components/Entry";
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <Entry />
+        <Entry />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="src/assets/globe.png" alt="travel logo" />
+      <h1>my travel journal.</h1>
+    </header>
+  );
+}
