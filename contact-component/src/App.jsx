c594246d84d6398a76b707436f58de6d22e64f5a
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-12-07 12:06:40 +0630

    updated contact component

--- contact-component/src/App.jsx
+++ contact-component/src/App.jsx
@@ -1,31 +1,35 @@
 import Contact from "./components/Contact";
+import mrWhiskerson from "./assets/mr-whiskerson.png";
+import fluffykins from "./assets/fluffykins.png";
+import felix from "./assets/felix.png";
+import pumpkin from "./assets/pumpkin.png";
 
 function App() {
   return (
     <div className="contacts">
       <Contact
-        img="src/assets/mr-whiskerson.png"
-        name="Mr. Whiskerson"
-        phone="(212) 555-1234"
-        email="mr.whiskaz@catnap.meow"
+        img={mrWhiskerson}
+        name={"Mr. Whiskerson"}
+        phone={"(212) 555-1234"}
+        email={"mr.whiskaz@catnap.meow"}
       />
       <Contact
-        img="src/assets/fluffykins.png"
-        name="Fluffykins"
-        phone="(212) 555-2345"
-        email="fluff@me.com"
+        img={fluffykins}
+        name={"Fluffykins"}
+        phone={"(212) 555-2345"}
+        email={"fluff@me.com"}
       />
       <Contact
-        img="src/assets/felix.png"
-        name="Felix"
-        phone="(212) 555-4567"
-        email="thecat@hotmail.com"
+        img={felix}
+        name={"Felix"}
+        phone={"(212) 555-4567"}
+        email={"thecat@hotmail.com"}
       />
       <Contact
-        img="src/assets/pumpkin.png"
-        name="Pumpkin"
-        phone="(0800) CAT KING"
-        email="pumpkin@scrimba.com"
+        img={pumpkin}
+        name={"Pumpkin"}
+        phone={"(0800) CAT KING"}
+        email={"pumpkin@scrimba.com"}
       />
     </div>
   );
