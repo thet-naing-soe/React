42b8e1f5a8caf5852de5bf4497c27f4b0eaf0156
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-12-07 12:06:03 +0630

    updated travel journal

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,14 +1,15 @@
-import "./App.css";
 import Header from "./components/Header";
 import Entry from "./components/Entry";
+import data from "./api/data";
+
 function App() {
+  const entryElements = data.map((entry) => {
+    return <Entry key={entry.id} {...entry} />;
+  });
   return (
     <>
       <Header />
-      <main className="container">
-        <Entry />
-        <Entry />
-      </main>
+      <main className="container">{entryElements}</main>
     </>
   );
 }
