174974ea59fc2c49e903ce23cf89c133f73f74cb
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-12-08 17:06:22 +0630

    chef claude

---
+++ chef-claude/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ chef-claude/src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <main>
+      <form action="" className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-level="Add ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+    </main>
+  );
+}
