00d5cbd5715f64332edff79bff074b862540c04d
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-12-09 21:41:39 +0630

    updated chef claude

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 export default function Main() {
+  const [ingredients, setIngredients] = useState([]);
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prev) => [...prev, newIngredient]);
+    // ingredients.push(newIngredient);,
+    //
+    //
+    // console.log(ingredients);
+  };
   return (
     <main>
-      <form action="" className="add-ingredient-form">
+      <form onSubmit={handleSubmit} action="" className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-level="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul>{ingredientsListItems}</ul>
     </main>
   );
 }
