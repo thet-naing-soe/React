be5c0971481ef6a6af09420b813d41221ad60c10
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-12-10 18:21:52 +0630

    updated joke component

--- joke-component/src/components/Joke.jsx
+++ joke-component/src/components/Joke.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 export default function Joke(props) {
+  const [isShown, setIsShown] = useState(false);
+  function toggleShown() {
+    setIsShown((prev) => !prev);
+  }
   return (
     <>
       {props.setup && <p className="setup">Setup: {props.setup}</p>}
-      <p className="punchline">Punchline: {props.punchline}</p>
+      {isShown ? <p className="punchline">Punchline: {props.punchline}</p> : null}
+      <button onClick={toggleShown}>
+        {isShown ? "Hide" : "Show"} punchline
+      </button>
       <hr />
     </>
   );
