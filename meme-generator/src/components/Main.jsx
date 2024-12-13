cc2f2abe094b10ff8751badeb0026ddbc0e19ab3
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-12-13 22:44:50 +0630

    meme-generator

---
+++ meme-generator/src/components/Main.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+
+export default function Main() {
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMeme, setAllMeme] = useState([]);
+  function handleChange(event) {
+    const { value, name } = event.currentTarget;
+    setMeme((prevMeme) => ({ ...prevMeme, [name]: value }));
+  }
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMeme(data.data.memes));
+  }, []);
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMeme.length);
+    const newMemeUrl = allMeme[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      imageUrl: newMemeUrl,
+    }));
+  }
+  return (
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            onChange={handleChange}
+            value={meme.topText}
+          />
+        </label>
+
+        <label>
+          Bottom Text
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            onChange={handleChange}
+            value={meme.bottomText}
+          />
+        </label>
+        <button onClick={getMemeImage}>Get a new meme image 🖼</button>
+      </div>
+      <div className="meme">
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
+      </div>
+    </main>
+  );
+}
